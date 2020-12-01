d25a7be967799a88a3c0d91e29198bfa99dda8ea
abhijit sinha <abhijit.sinha@imaginea.com>
2020-12-01 18:20:13 +0530

    add statewise daily data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "covid19",
   env: {
     PM2_SERVE_PATH: '.',
-    PM2_SERVE_PORT: 8080,
+    PM2_SERVE_PORT: 5000,
     PM2_SERVE_SPA: 'true',
     PM2_SERVE_HOMEPAGE: './index.html'
   }
